6387d791258cbb82900419d76fe3a08b53427f98
Felix Schneider <felix.schneider@arcor.de>
2020-05-09 18:25:11 +0200

    first rbind to get rid of the train/test duality; status for assignment task 5

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,18 @@ download.file(fileURL,"getdata_projectfiles_UCI_HAR_Dataset.zip")
 unzip("getdata_projectfiles_UCI_HAR_Dataset.zip")
 setwd("UCI HAR Dataset/")
 
-# 4. appropriately label the dataset with descriptive variable names
+X_train <- read.table("train/X_train.txt")
+X_test  <- read.table("test/X_test.txt")
+y_train <- read.table("train/y_train.txt",col.names="activity_code")
+y_test  <- read.table("test/y_test.txt",  col.names="activity_code")
+subject_train <- read.table("train/subject_train.txt",col.names="subject")
+subject_test  <- read.table("test/subject_test.txt",  col.names="subject")
+
+# 1. merge the training and test sets to create one data set
+X <- rbind(X_train,X_test)
+y <- rbind(y_train,y_test)
+subject <- rbind(subject_train,subject_test)
+
 features <- read.table("features.txt",stringsAsFactors=FALSE)
 features <- features[,2]
 pattern <- "mean[(][)]|std[(][)]"
@@ -14,25 +25,22 @@ features_cols <- grepl(pattern,features)
 features_names <- grep(pattern,features,value=TRUE)
 features_names <- gsub("[()]","",features_names)
 
-X_train <- read.table("train/X_train.txt",col.names=features,check.names=FALSE)
-X_test  <- read.table("test/X_test.txt",  col.names=features,check.names=FALSE)
 # 2. extract only the measurements of the mean and standard deviation for each measurement
-X_train <- X_train[,features_cols]
-X_test  <- X_test[,features_cols]
-names(X_train) <- features_names
-names(X_test)  <- features_names
+X <- X[,features_cols]
 
-y_train <- read.table("train/y_train.txt",col.names="activity_code",colClasses="character")
-y_test  <- read.table("test/y_test.txt",  col.names="activity_code",colClasses="character")
-
-subject_train <- read.table("train/subject_train.txt",col.names="subject")
-subject_test  <- read.table("test/subject_test.txt",col.names="subject")
-
-# 1. merge the training and test sets to create one dataset
-train <- cbind(subject_train,X_train,y_train)
-test  <- cbind(subject_test, X_test, y_test)
-
-har <- rbind(train,test)
-
+# 3. use descriptive activity names to name the activities in the data set
 activities <- read.table("activity_labels.txt",col.names=c("activity_code","activity_label"))
+y <- merge(y,activities,sort=FALSE)
 
+# 4. appropriately label the data set with descriptive variable names
+names(X) <- features_names
+
+# finally create one dataset
+har <- cbind(subject,X,y)
+
+# 5. create second, independent tidy data set with the average of each variable
+#    for each activity and each subject
+library(dplyr)
+har %>%
+  group_by(activity_label,subject) %>%
+  summarise(mean())
