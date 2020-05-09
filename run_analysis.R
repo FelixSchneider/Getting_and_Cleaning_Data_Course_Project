be8f04570fabaff5243dda064f6237885c8234f2
Felix Schneider <felix.schneider@arcor.de>
2020-05-09 17:52:46 +0200

    moved selection of 2nd column of features dataframe out of calls to grep and grepl

--- run_analysis.R
+++ run_analysis.R
@@ -8,20 +8,25 @@ setwd("UCI HAR Dataset/")
 
 # 4. appropriately label the dataset with descriptive variable names
 features <- read.table("features.txt",stringsAsFactors=FALSE)
-# 2. extract only the measurements of the mean and standard deviation for each measurement
+features <- features[,2]
 pattern <- "mean[(][)]|std[(][)]"
-features_cols <- grepl(pattern,features[,2])
-features_names <- grep(pattern,features[,2],value=TRUE)
+features_cols <- grepl(pattern,features)
+features_names <- grep(pattern,features,value=TRUE)
 features_names <- gsub("[()]","",features_names)
 
 X_train <- read.table("train/X_train.txt",col.names=features,check.names=FALSE)
 X_test  <- read.table("test/X_test.txt",  col.names=features,check.names=FALSE)
+# 2. extract only the measurements of the mean and standard deviation for each measurement
+X_train <- X_train[,features_cols]
+X_test  <- X_test[,features_cols]
+names(X_train) <- features_names
+names(X_test)  <- features_names
 
 y_train <- read.table("train/y_train.txt",col.names="activity_code",colClasses="character")
 y_test  <- read.table("test/y_test.txt",  col.names="activity_code",colClasses="character")
 
 subject_train <- read.table("train/subject_train.txt",col.names="subject")
-subject_test <- read.table("test/subject_test.txt",col.names="subject")
+subject_test  <- read.table("test/subject_test.txt",col.names="subject")
 
 # 1. merge the training and test sets to create one dataset
 train <- cbind(subject_train,X_train,y_train)
